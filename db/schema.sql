dd9ad4f1a5f2817a3bc7a676f50fdce7dc7052f0
Stephen Moore <stephenmoore33@outlook.com>
2021-07-12 21:19:53 -0400

    added tables campground_checklist, trail_checklist

--- db/schema.sql
+++ db/schema.sql
@@ -3,9 +3,29 @@ CREATE DATABASE wanderlog_db;
 
 USE wanderlog_db;
 
+CREATE TABLE campground_checklist (
+    campground_checklist_id NOT NULL AUTO_INCREMENT,
+    campground_checklist_name VARCHAR(70) NOT NULL,
+    campground_checklist_icon VARCHAR(70) NOT NULL
+    PRIMARY KEY (campground_checklist_id)
+)
+
+CREATE TABLE trail_checklist (
+    trail_checklist_id NOT NULL AUTO_INCREMENT,
+    trail_checklist_name VARCHAR(70) NOT NULL,
+    trail_checklist_icon VARCHAR(70) NOT NULL
+    PRIMARY KEY (trail_checklist_id)
+)
+
+
 CREATE TABLE campground_post (
-    id INT NOT NULL,
+    campground_post_id NOT NULL AUTO_INCREMENT,
+    created TIMESTAMP,
     published BOOLEAN NOT NULL, 
-    trip_start NOT NULL,
-    trip_end NOT NULL,
+    trip_start DATE NULL,
+    trip_end DATE NULL,
+    campground_name VARCHAR(255) NOT NULL,
+    location_city VARCHAR(255) NOT NULL,
+    location_state VARCHAR(60) NOT NULL,
+    PRIMARY KEY (campground_post_id)
 )
