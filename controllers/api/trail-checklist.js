b893d5761afb3d90a41e5cf6bf6cca3a11a7396e
hurondragon <bentzbry@gmail.com>
2021-07-17 14:02:00 -0400

    Adding more routes

---
+++ controllers/api/trail-checklist.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { Trail_checklist } = require('../../models');
+//Get ALL Trail Checlist items
+
+
+module.exports = router;
---
+++ controllers/api/trail-post.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const { Trail_post } = require('../../models');
+
+//Find All Trail Posts
+//Find One Trail Posts by :id
+//Delete Trail Post
+//Post Trail Post
+//Put Item in Trail Post
+
+module.exports = router;
