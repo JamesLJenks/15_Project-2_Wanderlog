ec2b3f99b22e179781c8f741b8561e03f968eae6
Jay <76748490+JamesLJenks@users.noreply.github.com>
2021-07-18 09:47:07 -0400

    Revert "Edit models index"

--- models/index.js
+++ models/index.js
@@ -1,8 +1,10 @@
-const CampgroundChecklist = require('./Campground_checklist');
-const CampgroundPost = require('./Campground_post');
-const Comment = require('./Comment');
-const TrailChecklist = require('./Trail_checklist');
-const TrailPost = require('./Trail_post');
-const User = require('./User');
+const { Model } = require("sequelize/types");
+
+const CampgroundChecklist = require('Campground_checklist');
+const CampgroundPost = require('Campground_post');
+const Comment = require('Comment');
+const TrailChecklist = require('Trail_checklist');
+const TrailPost = require('Trail_post');
+const User = require('User');
 
 module.exports = {CampgroundChecklist,CampgroundPost,Comment,TrailChecklist,TrailPost,User}
